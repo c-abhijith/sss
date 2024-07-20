01609b654dc5df5d895bd7bf21fc302bbca623a7
c-abhijith <cabhijithofficial@gmail.com>
2024-07-20 10:32:55 +0530

    al

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src="../../assets/insta.png" className="lal" alt="logo" />
+        
+    </div>
+  );
+}
+
+export default App;
