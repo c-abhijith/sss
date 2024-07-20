0a70895df2d165ff73c33e1241e7c56d4564bf71
c-abhijith <cabhijithofficial@gmail.com>
2024-07-20 10:48:17 +0530

    no

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <img src={`${process.env.PUBLIC_URL}/logo.png`}  className="lal" alt="logo" />
+        <img src={`${process.env.PUBLIC_URL}/assets/insta.png`}  className="lal" alt="logo" />
         
     </div>
   );
